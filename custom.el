1c23969c7bbe1e4c8948ce6ec103cba532e91d85
Eduard <elohmann@xs4all.nl>
2011-02-25 09:14:02 +0100

    Initial add of org-mode

--- custom.el
+++ custom.el
@@ -18,7 +18,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 131 :width normal :foundry "unknown" :family "Liberation Serif"))))
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "unknown" :family "Liberation Mono"))))
  '(background "blue")
  '(font-lock-builtin-face ((((class color) (background dark)) (:foreground "Turquoise"))))
  '(font-lock-comment-face ((t (:foreground "MediumAquamarine"))))
--- main.el
+++ main.el
@@ -3,6 +3,7 @@
 
 (add-to-list 'load-path "~/emacs.d/lisp")
 (add-to-list 'load-path "~/emacs.d/my")
+(add-to-list 'load-path "~/emacs.d/org/lisp")
 
 (load "general.el")
 (load "colors.el")
@@ -14,3 +15,12 @@
 (global-linum-mode 1)               ; line numbers everywhere
 
 (add-hook 'org-mode-hook 'turn-on-font-lock)
+
+(require 'org-install)
+(require 'org-mobile)
+;; Set to the location of your Org files on your local system
+(setq org-directory "~/Org")
+;; Set to the name of the file where new notes will be stored
+(setq org-mobile-inbox-for-pull "~/Org/index.org")
+;; Set to <your Dropbox root directory>/MobileOrg.
+(setq org-mobile-directory "~/Dropbox/MobileOrg")
