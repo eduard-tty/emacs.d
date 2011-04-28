0515434bb0976dad8b9bac7a7b8e49b70a96765b
Eduard <elohmann@xs4all.nl>
2011-04-28 22:23:46 +0200

    works here now, no clue why

--- main.el
+++ main.el
@@ -7,10 +7,6 @@
 
 (load "general.el")
 (load "colors.el")
-; aalto-light arjen bharadwaj clarity-and-beauty cooper-light2
-; dark-laptop gtk-ide high-contrast linh-dang-dark scintilla sitaram-nt
-; standard vim-colors xp
-(color-theme-standard)
 
 (load "markdown.el")
 ; (load "haskell.el")
--- my/colors.el
+++ my/colors.el
@@ -1,4 +1,7 @@
 (require 'color-theme)
 (setq color-theme-is-global t)
 (color-theme-initialize)
-
+; aalto-light arjen bharadwaj clarity-and-beauty cooper-light2
+; dark-laptop gtk-ide high-contrast linh-dang-dark scintilla sitaram-nt
+; standard vim-colors xp
+(color-theme-standard)
