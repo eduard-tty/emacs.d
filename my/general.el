ff32c790d9646894d64a5ec66d04ed7a4d8748fc
Eduard <eduard@tty.nl>
2011-06-17 10:14:26 +0200

    added revert-all-buffers

--- my/general.el
+++ my/general.el
@@ -1,7 +1,7 @@
 (global-font-lock-mode 1)
-(setq indent-tabs-mode nil)         ; spaces when indenting code
-(fset 'yes-or-no-p 'y-or-n-p)       ; replace y-e-s by y
-(setq inhibit-startup-message t)    ; no splash
+(setq-default indent-tabs-mode nil)         ; spaces when indenting code
+(fset 'yes-or-no-p 'y-or-n-p)               ; replace y-e-s by y
+(setq inhibit-startup-message t)            ; no splash
 
 (setq-default truncate-lines nil)
 (setq-default truncate-partial-width-windows nil)
@@ -9,8 +9,29 @@
 (tool-bar-mode -1)
 (menu-bar-mode 1)
 (set-scroll-bar-mode 'right)
-(setq scroll-step 1)                ; scroll one line when moving past top or bottom
+(setq scroll-step 1)                        ; scroll one line when moving past top or bottom
 (setq visible-bell t)
 
 (require 'linum)
-(global-linum-mode 1)               ; line numbers everywhere
+(global-linum-mode 1)                       ; line numbers everywhere
+
+
+
+
+; user defined functions
+
+(defun revert-all-buffers () "Replace text of all open buffers with the text of the corresponding visited file on disk."
+  (interactive)
+  (when (y-or-n-p "Revert all buffers? ")
+    (let* ((list (buffer-list)) (buffer (car list)))
+      (while buffer
+        (when (buffer-file-name buffer)
+          (set-buffer buffer)
+          (revert-buffer t t t)
+        )
+        (setq list (cdr list))
+        (setq buffer (car list))
+      )
+    )
+  )
+)
