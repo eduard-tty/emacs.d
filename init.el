5c9120600cbd3d3de0de6660f193bcbdfa40c949
Eduard Lohmann <eduard@tty.nl>
2013-06-28 18:01:46 +0200

    Use a custom file

--- init.el
+++ init.el
@@ -1,28 +1,6 @@
-(custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(auto-save-default nil)
- '(backup-by-copying nil)
- '(confirm-kill-emacs (quote yes-or-no-p))
- '(indent-tabs-mode nil)
- '(make-backup-files nil)
- '(menu-bar-mode nil)
- '(ps-font-size (quote (9 . 9)))
- '(ps-header-font-size (quote (8 . 10)))
- '(ps-header-title-font-size (quote (10 . 12)))
- '(ps-left-footer (quote (ps-get-buffer-name)))
- '(ps-line-number t)
- '(ps-line-number-start 1)
- '(ps-line-number-step 1)
- '(ps-lpr-command "xpp")
- '(ps-print-header-frame nil)
- '(scroll-bar-mode nil)
- '(tab-width 8)
- '(tool-bar-mode nil)
- '(transient-mark-mode t)
- )
+(setq custom-file "/home/eduard/emacs-git/eduard/custom_file.el")
+(load custom-file)
+
 
 (add-to-list 'load-path (concat user-emacs-directory (convert-standard-filename "my/")))
 (require 'elpa)
