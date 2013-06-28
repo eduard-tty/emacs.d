d2f23b8d226ecf90d8dd3a2fa06fbe0395bafe77
Eduard Lohmann <eduard@tty.nl>
2013-06-28 18:03:41 +0200

    remove experiments

--- init.el
+++ init.el
@@ -5,8 +5,4 @@
 (add-to-list 'load-path (concat user-emacs-directory (convert-standard-filename "my/")))
 (require 'elpa)
 
-
-;; (add-to-list 'load-path "/home/eduard/emacs-git/eduard/")
-;; (load "/home/eduard/emacs-git/eduard/after_elpa_init.el")
-;; (color-theme-solarized-dark)
 (add-hook 'after-init-hook (lambda () (load "/home/eduard/emacs-git/eduard/after_elpa_init.el")))
