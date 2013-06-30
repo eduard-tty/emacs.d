1b811b88f7107e8df214a0e260898fc439bea179
Eduard <eduard@tty.nl>
2013-06-30 20:45:26 +0200

    use custom menu to set font

--- my/appearance.el
+++ my/appearance.el
@@ -1,4 +1,4 @@
-(set-default-font '"-unknown-Inconsolata-normal-normal-normal-*-16-*-*-*-m-0-iso10646-1")
+;; (set-default-font '"-unknown-Inconsolata-normal-normal-normal-*-16-*-*-*-m-0-iso10646-1")
 
 (require 'linum)
 (global-linum-mode 1)
